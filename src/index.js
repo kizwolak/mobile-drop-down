b44c0daa851b5867f792fbbf063933aaca8cedd3
Krzysztof <ki.zwolak@gmail.com>
2022-12-14 02:42:53 +0100

    Main logic finished

--- src/index.js
+++ src/index.js
@@ -9,9 +9,19 @@ const close = document.querySelector('.close');
 mainbutton.addEventListener('click', () => {
     main.style.transition = '1s ease';
     main.style.transform = 'translateX(110%)';
+    setTimeout(main.style.display = 'none', 1000);
     menu.style.visibility = 'visible';
     menu.style.height = '100%;'
     menu.style.transition = '1s ease';
     menu.style.transform = 'translateX(0%)';
+});
 
+close.addEventListener('click', () => {
+    main.style.transition = '1s ease';
+    main.style.transform = 'translateX(0%)';
+    main.style.display = 'block';
+    menu.style.visibility = 'hidden';
+    menu.style.height = '100%;'
+    menu.style.transition = '1s ease';
+    menu.style.transform = 'translateX(-110%)';
 });
